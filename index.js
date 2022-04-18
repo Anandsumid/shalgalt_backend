a5d2fc7512a55637afd080e1eff330378698505f
lab_1_1x <lab_1_15@lab-1-15s-iMac.local>
2022-04-18 18:42:14 +0800

    haha

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const apiRoute = require("./routes/api");
 const dotenv = require("dotenv").config();
 const port = 3001;
 const connection = require("./database");
-const cors = require('./middleware/cors')
-
+const cors = require("cors")
 
 app.use(express.json());
 app.use(express.static("public"));
+app.use(cors())
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Content-Type", "application/json");
+
+  next();
+})
 app.use("/api", apiRoute);
 app.all("*", (req, res, next) => {
   res.status(404).json({
